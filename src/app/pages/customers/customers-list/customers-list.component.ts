323ff2cca11c5c18b9703d10a71d0be1005850da
enesbytmr <enesbytmr@gmail.com>
2024-09-16 23:15:25 +0300

    sidebar update

--- src/app/pages/customers/customers-list/customers-list.component.ts
+++ src/app/pages/customers/customers-list/customers-list.component.ts
@@ -5,7 +5,7 @@ import { Customer } from '../customer.model';
 @Component({
   selector: 'app-customers-list',
   templateUrl: './customers-list.component.html',
-  styleUrls: ['./customers-list.component.css']
+  styleUrls: ['./customers-list.component.scss']
 })
 export class CustomersListComponent implements OnInit {
   customers: Customer[] = [];
